89bf3bfdd1e8bf90400edbfe5232a1919d3f3768
auberamas <151127487+auberamas@users.noreply.github.com>
2025-05-17 00:39:36 +0200

    update

--- js/script.js
+++ js/script.js
@@ -21,17 +21,76 @@ function move(visible) {
 }
 */
 
-window.addEventListener('scroll', () => {
-  const blocs = document.querySelectorAll('.bloc');
-  const screenPosition = window.innerHeight;
+// ---  animate the title -------------------------
 
-  blocs.forEach(bloc => {
+window.addEventListener("DOMContentLoaded", () => {
+    const title = document.querySelector(".title");
+    const fullText = title.textContent;
+    const table = fullText.split("|"); // ["BLUE", "Bringing Life to Underwater Ecosystems"]
+    const container = title;
+    let index = 0;
+    let char = 0;
+    let direction = "write"; // or "erase"
+
+    container.textContent = ""; 
+
+    function animateText() {
+        const current = table[index];
+
+        if (direction === "write") {
+
+            // put the title in span tag to display it
+            // add a letter until the end of the length of the
+            if (char <= current.length) {                
+                container.innerHTML = `<span>${current.slice(0, char)}</span>`;
+                char++;
+                setTimeout(animateText, 70);
+            } 
+
+            // end of title reached => earse the title
+            else {
+                direction = "erase";
+                setTimeout(animateText, 1000); // pause to avoid a glitch
+            }
+        } 
+        else if (direction === "erase") {
+            
+            // retrive letter by letter the elment of the table
+            if (char >= 0) {
+                container.innerHTML = `<span>${current.slice(0, char)}</span>`;
+                char--;
+                setTimeout(animateText, 70);
+
+            } else {
+                direction = "write";
+                index = (index + 1) % table.length;
+                char = 0;
+                setTimeout(animateText, 500);
+            }
+        }
+    }
+    animateText();
+});
+
+
+
+// ---- to move the blocks when scrolling -----------------
+
+window.addEventListener('scroll', () => {
+    // get all the elments of the class bloc
+    const blocs = document.querySelectorAll('.bloc');
+
+    const screenPosition = window.innerHeight;
+
+    blocs.forEach(bloc => {
     const position = bloc.getBoundingClientRect().top;
 
     if (position < screenPosition - 100) {
-      bloc.classList.add('visible');
+        bloc.classList.add('visible');
     } else {
-      bloc.classList.remove('visible');
+        bloc.classList.remove('visible');
     }
-  });
+    });
 });
+
+
